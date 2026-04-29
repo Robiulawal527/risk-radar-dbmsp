8889a5a10d02fd2563978086639836336575b555
Robiul Awal <robiulawal527@gmail.com>
2026-04-29 22:48:00 +0600

    new version

--- src/app/components/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router';
-
-export default function Layout() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50">
-      <Outlet />
-    </div>
-  );
-}
